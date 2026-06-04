8be412250468b95f207e9a3108ecbd7864617f56
gdg0123 <gdg012377@gmail.com>
2026-06-04 21:41:12 +0900

    Merge 시스템 만드는 중

---
+++ My project/Assets/Script/MergeGameScripts/MergeGameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MergeGameManager : MonoBehaviour
+{
+    [Header("아이템 설정")]
+    public GameObject[] materialPrefabs;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
